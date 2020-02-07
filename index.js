ce32c98a2b9615905a5600335a8e96291730de81
Kapil Bindal <kapilbindal1@gmail.com>
2020-02-07 10:54:16 +0530

    Readme added

--- index.js
+++ index.js
@@ -5,65 +5,14 @@ const Tx = require('ethereumjs-tx');
 const EthUtil = require('ethereumjs-util');
 const Bip39 = require('bip39');
 const Hdkey = require('hdkey');
-const { EncryptedKeystoreV3Json } = require('web3-core');
-// const { IAccount, INodeRecord } from '~/redux/account/types';
 const keythereum = require('keythereum');
 const BigInt = require('big-integer');
 const { contractFunctions } = require('./constants');
 
 const REACT_APP_API_URL_WEB3 = 'https://rpc.fantom.network/'
 const REACT_APP_API_URL_FANTOM = 'https://api.fantom.network/api/v1/'
-const { API_URL_FANTOM, KEY_INFURA } = process.env;
-// export const DEFAULT_PROVIDERS: INodeRecord[] = [
-//   { address: REACT_APP_API_URL_WEB3 || '' },
-//   { address: 'ws://18.189.195.64:4501' },
-//   { address: 'ws://18.191.96.173:4502' },
-//   { address: 'ws://3.15.138.107:4500' },
-// ];
-
-// const Web3 = require("web3");
-// const Tx = require("ethereumjs-tx");
-// type Transfer = {
-// 	from: string;
-// 	to: string;
-// 	value: string;
-// 	memo: string;
-// 	privateKey: string;
-// 	gasLimit?: number;
-// 	web3Delegate?: any;
-// 	cb?: () => {};
-// };
-const URL_FANTOM = API_URL_FANTOM;
-const URL_ETHEREUM = `https://rinkeby.infura.io/v3/${KEY_INFURA}`;
-
-// export interface ITransfer {
-// 	from: string;
-// 	to: string;
-// 	value: string;
-// 	memo: string;
-// 	privateKey: string;
-// 	gasLimit?: number;
-// 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// 	web3Delegate?: any;
-// }
-// const web3 = new Web3(new Web3.providers.HttpProvider(API_URL_FANTOM));
-// const code = web3.eth
-//   .getCode("0xfa00face00fc0000000000000000000000000100")
-//   .then(res => console.log("code res: ", res))
-//   .catch(e => console.log("code e: ", e));
-// console.log("code: ", code);
-
-
-
 
 let web3 = new Web3(new Web3.providers.HttpProvider(REACT_APP_API_URL_WEB3 || ''));
-let sfc = null;
-
-// class Web3Agent {
-// constructor() {
-// 	this.web3 = new Web3(new Web3.providers.HttpProvider(REACT_APP_API_URL_WEB3 || ''));
-// }
-
 
 const getBalance = async (address) => {
 	const res = await web3.eth.getBalance(address);
@@ -89,7 +38,6 @@ const getKeystore = (privateKey, password) => {
 
 	return web3.eth.accounts.encrypt(privateKey, password);
 };
-// Get info on delegator
 
 const getDelegate = (from, delegateAddress, sfc) => {
 	return new Promise(resolve => {
@@ -112,7 +60,6 @@ const getPrivateKey = (keystore, password) =>
 			resolve(dataRes instanceof Buffer ? EthUtil.bufferToHex(dataRes) : null);
 		})
 	);
-// Get current epoch
 
 const getCurrentEpoch = (from, sfc) => {
 	return new Promise(resolve => {
@@ -125,30 +72,8 @@ const getCurrentEpoch = (from, sfc) => {
 	});
 }
 
-// async deligateUnstake({ publicKey }) {
-//   const web3 = new Web3(
-//     new Web3.providers.HttpProvider(REACT_APP_API_URL_WEB3 || '')
-//   );
-
-//   const sfc = new web3.eth.Contract(
-//     contractFunctions,
-//     '0xfc00face00000000000000000000000000000000'
-//   );
-//   sfc.methods
-//     .prepareToWithdrawDelegation()
-//     .call({ from: publicKey }, function(error, result) {
-//       console.log(result, '***sdfksfsd');
-//       console.log(error, '***sdfksfsd1');
-//       if (result) {
-//         return true;
-//       }
-//       return false;
-//     });
-// }
 
 const estimateFee = async ({ from, to, value, memo }) => {
-	// if (!this.web3 || !(await this.isConnected()))
-	//   throw new Error('Not connected');
 
 	const gasPrice = await web3.eth.getGasPrice();
 	const gasLimit = await web3.eth.estimateGas({
@@ -166,21 +91,10 @@ const estimateFee = async ({ from, to, value, memo }) => {
 
 	return fee;
 }
-// Get info on delegator
 
 const getDelegationPendingRewards = async (from, delegateAddress) => {
 	const web3 = new Web3(new Web3.providers.HttpProvider(REACT_APP_API_URL_WEB3 || ''));
 	const sfc = new web3.eth.Contract(contractFunctions, '0xfc00face00000000000000000000000000000000');
-	// sfc.methods
-	//   .delegations("0x2210BE0bDba6daC30c4023Ea22b4235E420178bE")
-	//   .call({ from: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe" }, function(
-	//     error,
-	//     result
-	//   ) {
-	//     console.log("hello", error);
-	//     console.log(result);
-	//   });
-	// Get delegator info and current epoch - 1 (i.e the previous sealed epoch)
 	const info = await Promise.all([
 		getCurrentEpoch(from, sfc),
 		getDelegate(from, delegateAddress, sfc) || {}
@@ -205,21 +119,8 @@ const getDelegationPendingRewards = async (from, delegateAddress) => {
 
 const delegateStake = ({ amount, publicKey, privateKey, validatorId, isWeb = false }) => {
 	console.log(amount, publicKey, privateKey, validatorId, '******8amount, publicKey, privateKey, validatorId');
-
 	const web3 = new Web3(new Web3.providers.HttpProvider(REACT_APP_API_URL_WEB3 || ''));
-
 	const web3Sfc = new web3.eth.Contract(contractFunctions, '0xfc00face00000000000000000000000000000000');
-
-	// Assign contract functions to sfc variable
-	// tx = this.sfc.createDelegation("1", {
-	//   from: "0x2122ecA57D8F5Ca902363CbA9d256A66C7664332",
-	//   value: "1"
-	// });
-	// const sfc = new this.web3.eth.Contract(
-	//   abi,
-	//   "0xfc00face00000000000000000000000000000000"
-	// );
-	// const am = Number(amount)
 
 	return transfer({
 		from: publicKey,
@@ -230,9 +131,7 @@ const delegateStake = ({ amount, publicKey, privateKey, validatorId, isWeb = fal
 		gasLimit: 200000,
 		web3Delegate: web3,
 		isWeb
-		// cb: data,
 	});
-	// this.sfc.stakersNum(); // if everything is all right, will return non-zero value
 }
 
 const restoreWallet = async (privateKey) => {
@@ -242,7 +141,6 @@ const restoreWallet = async (privateKey) => {
 
 const getTransactionFee = async (gasLimit) => {
 	const gasPrice = await web3.eth.getGasPrice();
-	// const gasLimit = 200000;
 	const fee = Web3.utils.fromWei(
 		BigInt(gasPrice.toString())
 			.multiply(BigInt(gasLimit.toString()))
@@ -264,7 +162,6 @@ const transfer = async ({
 	const useWeb3 = web3Delegate || web3;
 	const nonce = await useWeb3.eth.getTransactionCount(from);
 	const gasPrice = await useWeb3.eth.getGasPrice();
-	// const amount = parseFloat(value)
 
 	const rawTx = {
 		from,
@@ -275,27 +172,14 @@ const transfer = async ({
 		nonce: Web3.utils.toHex(nonce),
 		data: memo
 	};
-	console.log(new Buffer(privateKey), '****sdkksdfk')
-
-
-	console.log('*****privateKey', privateKey, typeof privateKey)
-
-	const bufferData = EthUtil.addHexPrefix(privateKey)
-	console.log(bufferData, '****sdjasdj')
 	const privateKeyBuffer = EthUtil.toBuffer(privateKey);
-	console.log(privateKeyBuffer, '****privateKeyBuffer')
 	const tx = new Tx(rawTx);
 	tx.sign(privateKeyBuffer);
 	const serializedTx = tx.serialize();
 	const res = await useWeb3.eth.sendSignedTransaction(`0x${serializedTx.toString('hex')}`);
 	if (isWeb) {
 		localStorage.setItem('txHash', res.transactionHash);
-
-
 	}
-	// if (cb) {
-	//   cb(res.transactionHash || '')
-	// }
 	return res;
 }
 
@@ -311,13 +195,11 @@ const delegateUnstake = async (publicKey, privateKey, isWeb = false) => {
 		gasLimit: 200000,
 		web3Delegate: web3,
 		isWeb
-		// cb: () => '',
 	});
 }
 
 const withdrawDelegateAmount = async (publicKey, privateKey, isWeb = false) => {
 	const web3 = new Web3(new Web3.providers.HttpProvider(REACT_APP_API_URL_WEB3 || ''));
-
 	const web3Sfc = new web3.eth.Contract(contractFunctions, '0xfc00face00000000000000000000000000000000');
 	return transfer({
 		from: publicKey,
@@ -328,7 +210,6 @@ const withdrawDelegateAmount = async (publicKey, privateKey, isWeb = false) => {
 		gasLimit: 200000,
 		web3Delegate: web3,
 		isWeb,
-		// cb: () => '',
 	});
 }
 
@@ -374,19 +255,6 @@ const estimateFeeMobile = async (value) => {
 	return fee;
 }
 
-// from debug network
-/* eslint-disable no-undef */
-// $FlowFixMe
-// GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
-// export const  {
-//   Fantom: new Web3Agent(),
-// };
-
-// console.log(web3, '*****asdjasdweb3')
-
-// const Fantom = new Web3Agent();
-
-// module.exports.Fantom = Fantom;
 module.exports.estimateFeeMobile = estimateFeeMobile
 module.exports.getBalance = getBalance
 module.exports.getKeystore = getKeystore
